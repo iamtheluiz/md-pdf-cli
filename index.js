d9d4b2a79dd29f1736647ba1b8de7ea135225e8f
iamtheluiz <iamtheluiz.dev@gmail.com>
2020-12-10 15:24:44 -0300

    feat: convert md to html

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 require('dotenv').config();
 const fs = require('fs');
-const path = require('path');
+const path = require('path')
+const { Remarkable } = require('remarkable');
 
-// Get all MD files from folder
+var md = new Remarkable('full', {
+  html: true,
+  typographer: true
+});
+
+// Get all files from folder
 const files = fs.readdirSync(process.env.INPUT_DIR);
 
-console.log(files);
-
+// Filter MD files
 const sanitizedFiles = files.filter(file => {
   const ext = path.extname(file);
 
@@ -15,4 +20,19 @@ const sanitizedFiles = files.filter(file => {
   }
 });
 
-console.log(sanitizedFiles);
+console.log(`Processing...`);
+
+sanitizedFiles.map(async file => {
+  const filePath = path.join(process.env.INPUT_DIR, file);
+  const fileOutputPath = path.join(process.env.OUTPUT_DIR, `${path.basename(file).split('.')[0]}.pdf`);
+
+  // Get file content
+  const content = fs.readFileSync(filePath);
+
+  // Convert md to HTML
+  const html = md.render(content.toString());
+
+  console.log(` - Complete => File: ${file}`);
+});
+
+console.log('Complete!');
