8abdb9c2d8ef9f76b811e3556a9ede15012f45ad
iamtheluiz <iamtheluiz.dev@gmail.com>
2020-12-23 14:50:43 -0300

    feat: export modules from 'lib/index.ts'

---
+++ lib/index.ts
@@ -0,0 +1,13 @@
+import addStylesToHtmlString from './addStylesToHtmlString';
+import convertContentToHtml from './convertContentToHtml';
+import convertImagePathsToBase64 from './convertImagePathsToBase64';
+import createPdfFromHtml from './createPdfFromHtml';
+import getMarkdownFilesFromFolder from './getMarkdownFilesFromFolder';
+
+export {
+  addStylesToHtmlString,
+  convertContentToHtml,
+  convertImagePathsToBase64,
+  createPdfFromHtml,
+  getMarkdownFilesFromFolder
+};
